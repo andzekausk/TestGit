3d528041cc1eb6c009175db4541ffcb3deed6ea4
andzekausk <s23kausandz@venta.lv>
2023-12-11 13:22:00 +0200

    :^)

---
+++ TestJavaFx/src/module-info.java
@@ -0,0 +1,5 @@
+module TestJavaFx {
+	requires javafx.controls;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
---
+++ TestJavaFxGit/src/module-info.java
@@ -0,0 +1,5 @@
+module TestJavaFxGit {
+	requires javafx.controls;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
